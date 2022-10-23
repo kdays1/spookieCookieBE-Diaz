560f705442d3fa0ea037f83d5a457846171ef4b0
kdays1 <caroodzg@gmail.com>
2022-10-22 20:16:37 -0500

    Desafio 2: Manejo de Archivos JS

--- desafio1.js
+++ desafio1.js
@@ -1,3 +1,10 @@
+var fs = require('fs')
+const { json } = require('stream/consumers')
+
+function callb(these, objectId) {
+    return objectId;
+}
+
 class Contenedor {
     #objectsContained
 
@@ -6,34 +13,115 @@ class Contenedor {
     }
 
     save(theObject) {
-        this.#objectsContained.push(theObject)
+        // this.#objectsContained.push(theObject)
+        fs.readFile('theFile.json', function (err,data) {
+            if (err) {
+                console.log('There was an error while reading the file');
+            } else {
+                //extracting info and pushing new object
+                var productsInFile = JSON.parse(data);
+                productsInFile = productsInFile.Productos;
+                theObject.id = productsInFile.length;
+                productsInFile.push(theObject);
+                console.log(productsInFile);
+                console.log(JSON.stringify(productsInFile));
+                //writing info to file
+                fs.writeFile('theFile.json', '{"Productos" :' + JSON.stringify(productsInFile) + '}', error => {
+                    if (error) {
+                        console.log('There was an error when saving the info');
+                    } else {
+                        console.log('The product with ID: '+ theObject.id + ' was saved :)');
+                    }
+                })
+            }
+        })
     }
-    getById(theId){
-        return this.#objectsContained.filter(x => x.id == theId)
+
+    getById(theId) {
+        // return this.#objectsContained.filter(x => x.id == theId)
+        // const content = () => fs.readFile('theFile.json', function (err,data) {
+        //     if (err) {
+        //         console.log('There was an error while reading the file');
+        //     } else {
+        //         //extracting info and getting the required object
+        //         var productsInFile = JSON.parse(data);
+        //         productsInFile = productsInFile.Productos;
+        //         console.log(productsInFile);
+        //         console.log(productsInFile.filter(x => x.id == theId));
+        //         var contents = productsInFile.filter(x => x.id == theId);
+        //     }
+        // })
+        // return content;
+        const content = () => fs.readFileSync('theFile.json')
+        var productsInFile = JSON.parse(content());
+        productsInFile = productsInFile.Productos;
+        var contents = productsInFile.filter(x => x.id == theId);
+        return contents;
     }
+
     getAll(){
-        return this.#objectsContained
+        // return this.#objectsContained
+        const content = () => fs.readFileSync('theFile.json')
+        var productsInFile = JSON.parse(content());
+        return productsInFile.Productos;
     }
     deleteByID(theId){
-        this.#objectsContained = this.#objectsContained.filter(x => x.id != theId)
+        // this.#objectsContained = this.#objectsContained.filter(x => x.id != theId)
+        fs.readFile('theFile.json', function (err,data) {
+            if (err) {
+                console.log('There was an error while reading the file');
+            } else {
+                //extracting info and pushing new object
+                var productsInFile = JSON.parse(data);
+                productsInFile = productsInFile.Productos;
+                productsInFile = productsInFile.filter(x => x.id != theId);
+                console.log('estos: ', productsInFile);
+                //writing info to file
+                fs.writeFile('theFile.json', '{"Productos" :' + JSON.stringify(productsInFile) + '}', error => {
+                    if (error) {
+                        console.log('There was an error when saving the info');
+                    } else {
+                        console.log('The product with ID: '+ theId + ' was deleted :)');
+                    }
+                })
+            }
+        })
     }
     deleteAll(){
-        this.#objectsContained = []
+        // this.#objectsContained = []
+        fs.writeFile('theFile.json', '', error => {
+            if (error) {
+                console.log('There was an error when deleting all files');
+            } else {
+                console.log('All the products were deleted');
+            }
+        })
     }
 }
 
+
 const productos = new Contenedor()
 
-productos.save({id: 0, title: 'Chamarra Rosa', price: 250, thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUPAGmmm_nF83rB0WxsLsECxgUWqeGp5CoDw&usqp=CAU'})
-productos.save({id: 1, title: 'Chamarra Azul', price: 150, thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKvevSuf65KREl9xg2kY2IeSBRdMIi7df_cw&usqp=CAU'})
+// Function save
 
-console.log('getAll', productos.getAll())
-console.log('getByID', productos.getById(1))
+productos.save({title: 'Chamarra Guinda', price: 150, thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUPAGmmm_nF83rB0WxsLsECxgUWqeGp5CoDw&usqp=CAU'});
 
-productos.deleteByID(1)
 
-console.log('deleteById', productos.getAll())
+// Function getById
 
-productos.deleteAll()
+    var gettingObject = productos.getById(1);
+    console.log('getByID', gettingObject);
 
-console.log('deleteAll', productos.getAll())
+//Function getAll
+
+    var gettingAllObjects = productos.getAll();
+    console.log('getAll', gettingAllObjects)
+
+
+//Function deleteBy
+
+setTimeout(productos.deleteByID, 300, 0);
+
+//Function deleteAll
+
+setTimeout(productos.deleteAll, 600);
