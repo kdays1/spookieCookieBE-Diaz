29f80afaf9945b43ad78759bf29cf78250d44514
kdays1 <caroodzg@gmail.com>
2022-10-27 22:19:39 -0600

    Desafio 3: servidor con express

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const { Console } = require('console');
-var fs = require('fs')
+let fs = require('fs')
 const { json } = require('stream/consumers')
-
-function callb(these, objectId) {
-    return objectId;
-}
+// const http = require ('http')
+// const connectedServer = server.listen(8080, () => {
+//     console.log('Servidor Http escuchando en el puerto ${connectedServer.address().port}')
+// })
+const express = require('express')
+const app = express()
+const PORT = 8080
 
 class Contenedor {
     #objectsContained
@@ -58,21 +61,13 @@ class Contenedor {
 
     async getAll(){
         // return this.#objectsContained
-        const productsPromise = await new Promise ((resolve, reject) => {
-            return fs.readFile('theFile.json', function (err,data) {
-                if (err) {
-                    console.log('There was an error while reading the file');
-                    return reject()
-                } else {
-                var productsInFile = JSON.parse(data);
-                productsInFile = productsInFile.Productos;
-                return resolve(productsInFile);
-                }
-            });
-        })
-        console.log('getAllPromise',productsPromise);
-        return productsPromise;
+        let productsPromise = await fs.promises.readFile('theFile.json', 'utf-8');
+        const products = await JSON.parse(productsPromise);
+        console.log(products.Productos)
+        console.log('LENGTH', products.Productos.length)
+        return (products.Productos);
     }
+
     deleteByID(theId){
         // this.#objectsContained = this.#objectsContained.filter(x => x.id != theId)
         fs.readFile('theFile.json', function (err,data) {
@@ -107,39 +102,36 @@ class Contenedor {
     }
 }
 
-
 const productos = new Contenedor()
 
 // Function save
 
-productos.save({title: 'Chamarra Guinda', price: 150, thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUPAGmmm_nF83rB0WxsLsECxgUWqeGp5CoDw&usqp=CAU'});
+// productos.save({title: 'Chamarra Guinda', price: 150, thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUPAGmmm_nF83rB0WxsLsECxgUWqeGp5CoDw&usqp=CAU'});
 
 
 // Function getById
-    // async function getPRoductById() {
-    //     try {
-    //         var gettingObject = await productos.getById(1);
-    //     }
-    //     catch(err){
-    //         console.log('error')
-    //     }
-    //     console.log('getById', gettingObject);
-    // }
-    // getPRoductById()
-    let gettingObject = productos.getById(1);
+
+    // let gettingObject = productos.getById(1);
 
 //Function getAll
 
     var gettingAllObjects = productos.getAll();
 
 
-//Function deleteBy
+app.get('/productos', async (req,res) => {
+    // console.log('La peticion fue enviada', gettingAllObjects)
+    let products = await productos.getAll();
+    res.json(JSON.stringify({products}))
+})
 
-setTimeout(productos.deleteByID, 300, 0);
+app.get('/productoRandom', async (req,res) => {
+    let rand = Math.floor(Math.random() * 3)
+    let product = await productos.getById(rand);
+    res.json(JSON.stringify({product}))
+})
 
-//Function deleteAll
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+})
 
-setTimeout(productos.deleteAll, 600);
-
-console.log('getById', gettingObject);
-console.log('getAll', gettingAllObjects);
+server.on("error", error => console.log(`Error en servidor ${error}`))
